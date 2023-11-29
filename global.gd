e0c282f8531b0894fd48959d6e80d3a09343c38a
Justin Deogrades <deog0004@algonquinlive.com>
2023-11-29 17:27:24 -0500

    Fixed a bug where the camera could be in a room the player is not by reducing the room detector to 1 pixel, added a cheat to grant all abilities, and polished some scene warps.

--- global.gd
+++ global.gd
@@ -10,12 +10,12 @@ var red_level_scene_path = ("res://Levels/RedLevel.tscn");
 var blue_level_scene_path = ("res://Levels/BlueLevel.tscn");
 var purple_level_scene_path = ("res://Levels/PurpleLevel.tscn");
 
-var warp_pos = Vector2(0,0);
+var warp_pos = Vector2(-36, 56);
 var warp_scene_path = " ";
 var warp_dir = 0;
 var dash_on_warp = false;
 var main_menu_created = false;
-var ability_unlocked = [true, true, true];
+var ability_unlocked = [false, false, false];
 var orbs_collected = 0;
 var orbs_to_spawn_id = [true, true, true, true, true, true, true, true]
 var portal_active = false;
@@ -27,4 +27,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ability_cheat"):
+		print_debug("Cheat worked");
+		for i in 3:
+			ability_unlocked[i] = true;
